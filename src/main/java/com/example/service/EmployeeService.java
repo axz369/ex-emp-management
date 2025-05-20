6ab88eb5725fc2c74e0eeb7bf0e2f626edbae9b4
axz369 <gp21a169@oecu.jp>
2025-05-20 14:14:34 +0900

    EmployeeServiceとその中にshowListメソッドの作成

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 従業員関連機能の業務処理を行うサービス.
+ */
+@Service
+@Transactional
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    /**
+     * 従業員情報を全件取得する.
+     *
+     * @return 取得した全従業員のリスト
+     */
+    public List<Employee> showList(){
+        //検索した従業員一覧を返却
+        return employeeRepository.findAll();
+    }
+}
