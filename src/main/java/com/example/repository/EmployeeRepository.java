8375b254c2fa75d51b39429d88ff5be582f83ebd
axz369 <gp21a169@oecu.jp>
2025-05-20 11:39:06 +0900

    EmployeeRepositoryクラスの作成

---
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -0,0 +1,55 @@
+package com.example.repository;
+
+import com.example.domain.Administrator;
+import com.example.domain.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    /**
+     * Employeeオブジェクトを生成するローマッパー.
+     */
+    private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
+        Employee employee = new Employee();
+        employee.setId(rs.getInt("id"));
+        employee.setName(rs.getString("name"));
+        employee.setImage(rs.getString("image"));
+        employee.setGender(rs.getString("gender"));
+        employee.setHireDate(rs.getDate("hire_date"));
+        employee.setMailAddress(rs.getString("mail_address"));
+        employee.setZipCode(rs.getString("zip_code"));
+        employee.setAddress(rs.getString("address"));
+        employee.setTelephone(rs.getString("telephone"));
+        employee.setSalary(rs.getInt("salary"));
+        employee.setCharacteristics(rs.getString("characteristics"));
+        employee.setDependentsCount(rs.getInt("dependents_count"));
+        return employee;
+    };
+
+
+
+    /**
+     * 従業員一覧情報を入社日順(降順)で取得します.
+     *
+     * @return 全従業員一覧 従業員が存在しない場合はサイズ0件の従業員一覧を返す
+     */
+    public List<Employee> findAll() {
+        String sql = "SELECT " +
+                        "id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count " +
+                        "FROM employees " +
+                        "ORDER BY hire_date desc " +
+                        ";";
+
+        List<Employee> employeeList = template.query(sql,EMPLOYEE_ROW_MAPPER);
+        return employeeList;
+    }
+}
